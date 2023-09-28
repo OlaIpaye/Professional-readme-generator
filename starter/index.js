3151b86faf5ccaf09ceac4841ca3a5328fd37ad6
Ola Ipaye <ipayeola@outlook.com>
2023-09-29 00:56:04 +0100

    Installed inquirer, updated index.js page with code.

--- starter/index.js
+++ starter/index.js
@@ -17,13 +17,95 @@ const generateMarkdown = `
 `;
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: projectTitle,
+    message: "What is the title of your project:",
+  },
+  {
+    type: "input",
+    name: projectDescription,
+    message: "Write a brief description for your project:",
+  },
+  {
+    type: "input",
+    name: tableOfContents,
+
+    type: "input",
+    type: appInstallationGuide,
+    message: "How do you want people to install your app?",
+
+    type: "input",
+    type: appUsage,
+    message: "How should people use your app?",
+
+    type: "input",
+    type: appLicense,
+    message: "License:",
+
+    type: "input",
+    type: appContributors,
+    message: "Contributors:",
+
+    type: "input",
+    type: appTesting,
+    message: "Testing:",
+  },
+  {
+    type: "input",
+    name: questions,
+    message:
+      "If you have any questions, please email our team: ipayeola@gmail.com",
+  },
+];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+fs.writeFile("README.md", generateMarkdown, (err) =>
+  err
+    ? console.error("Error writing your README file:", err)
+    : console.log("Your README.md file has been generated successfully.")
+);
 
 // function to initialize program
 function init() {}
+
+function generateReadme(answers) {
+  return `
+  # ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## License
+  This project is licensed under the ${answers.license} license.
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.tests}
+  
+  ## Questions
+  If you have any questions, please feel free to [email me](mailto:${answers.email}).
+  
+  ![GitHub Profile Picture](${answers.avatar_url})
+  `;
+}
 
 // function call to initialize program
 init();
